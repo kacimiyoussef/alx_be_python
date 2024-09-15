794c197874975eae0db481b8d30732c5e9888fc2
Youssef EL KASSIMI <ussef2ussef1@gmail.com>
2024-09-15 14:35:54 +0100

    change the code for promet user to input courent age

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,8 @@
-currentAge = int(input("How old are you?: "))  # Convert input to integer
+# Prompt the user to input their current age
+current_age = int(input("How old are you? "))
 
-age = currentAge + 27
+# Calculate the age in 2050
+age_in_2050 = current_age + 27
 
-print(f"In 2050, you will be {age} years old.")
+# Print the result
+print(f"In 2050, you will be {age_in_2050} years old.")
